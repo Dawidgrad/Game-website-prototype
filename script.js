fb166240d125a6753bd95d438d04c4a58e770cc1
Dawid Grad <dawidgrad.work@gmail.com>
2019-01-01 16:20:50 +0000

    Changed labels and placeholders for forms, added form validation for Account settings and registration

--- script.js
+++ script.js
@@ -86,17 +86,27 @@ function checkIfLoggedIn()
 
 
 /***********************************
-            Message boxes
+               Forms
 ***********************************/
 
 function changeEmail()
 {
-    alert("Email changed successfully!");
+    var success = changeEmailValidation();
+    
+    if (success)
+    {
+        alert("Email changed successfully!");
+    }
 }
 
 function changePassword()
 {
-    alert("Password changed successfully!");
+    var success = changePasswordValidation();
+    
+    if (success)
+    {
+        alert("Password changed successfully!");
+    }
 }
 
 function purchasePoints()
@@ -106,17 +116,32 @@ function purchasePoints()
 
 function changeNotifications()
 {
-    alert("Notifications preference changed successfully!");
+    var success = changeNotificationsValidation();
+    
+    if (success)
+    {
+        alert("Notifications preference changed successfully!");
+    }
 }
 
 function changeSecretQuestion()
 {
-    alert("Secret question changed successfully!");
+    var success = changeSecretQuestionValidation();
+    
+    if (success)
+    {
+        alert("Secret question changed successfully!");
+    }
 }
 
 function addTicket()
 {
     alert("Ticket has been added successfully!");
+}
+
+function addTicketReply()
+{
+    
 }
 
 function checkTicket()
@@ -137,6 +162,16 @@ function checkTicket()
     return false;
 }
 
+function register()
+{
+    var success = registerValidation();
+    
+    if (success)
+    {
+        alert("Registration has been successful!");
+    }
+}
+
 
 /***********************************
     Dynamic addition of elements
@@ -151,4 +186,157 @@ function addFieldTicket()
     input.style.margin = "3px";
     container.append(input);
     container.append(document.createElement("br"));
+}
+
+/***********************************
+            Validation
+***********************************/
+function registerValidation()
+{    
+    var username = document.forms["registrationForm"]["login"];
+    var password = document.forms["registrationForm"]["password"];
+    var confirmPassword = document.forms["registrationForm"]["confirmPassword"];
+    var email = document.forms["registrationForm"]["email"];
+    var secretAnswer = document.forms["registrationForm"]["secretAnswer"];
+    
+    if (username.value.length < 4 || username.value.length > 16)
+    {
+        alert("Username has to be 4 to 16 characters long!");
+        username.focus();
+        return false;
+    }
+    if (password.value.length < 8 || password.value.length > 16)
+    {
+        alert("Password has to be 8 to 16 characters long!");
+        password.focus();
+        return false;
+    }
+    if (password.value != confirmPassword.value)
+    {
+        alert("Passwords do not match!");
+        password.focus();
+        return false;
+    }
+    if (validateEmail(email.value) == false)
+    {
+        alert("Invalid email!");
+        email.focus();
+        return false;
+    }
+    if (secretAnswer.value.length < 4 || secretAnswer.value.length > 16)
+    {
+        alert("Secret answer has to be 4 to 16 characters long!");
+        secretAnswer.focus();
+        return false;
+    }
+    
+    return true;
+}
+
+function changeEmailValidation()
+{
+    var currentEmail = document.forms["changeEmailForm"]["currentEmail"];
+    var newEmail = document.forms["changeEmailForm"]["newEmail"];
+    var newEmailConfirm = document.forms["changeEmailForm"]["newEmailConfirm"];
+    
+    if (validateEmail(currentEmail.value) == false)
+    {
+        alert("Invalid current email!");
+        currentEmail.focus();
+        return false;
+    }
+    if (validateEmail(newEmail.value) == false)
+    {
+        alert("Invalid new email!");
+        newEmail.focus();
+        return false;
+    }
+    if (newEmail.value != newEmailConfirm.value)
+    {
+        alert("Emails don't match!");
+        newEmail.focus();
+        return false;
+    }
+    if (currentEmail.value == newEmail.value)
+    {
+        alert("Current and new emails are the same!");
+        newEmail.focus();
+        return false;
+    }
+    
+    return true;
+}
+
+function changePasswordValidation()
+{
+    var currentPassword = document.forms["changePasswordForm"]["currentPassword"];
+    var newPassword = document.forms["changePasswordForm"]["newPassword"];
+    var newPasswordConfirm = document.forms["changePasswordForm"]["newPasswordConfirm"];
+    
+    if (currentPassword.value.length < 8 || currentPassword.value.length > 16)
+    {
+        alert("Current password is invalid!");
+        currentPassword.focus();
+        return false;
+    }
+    if (newPassword.value.length < 8 || newPassword.value.length > 16)
+    {
+        alert("New password has to be 8 to 16 characters long!");
+        newPassword.focus();
+        return false;
+    }
+    if (newPassword.value != newPasswordConfirm.value)
+    {
+        alert("Passwords don't match!");
+        newPassword.focus();
+        return false;
+    }
+    if (currentPassword.value == newPassword.value)
+    {
+        alert("Current and new passwords are the same!");
+        currentPassword.focus();
+        return false;
+    }
+    
+    return true;
+}
+
+function changeNotificationsValidation()
+{
+    var receiveNotifications = document.forms["notificationSettingsForm"]["receiveNotifications"];
+    
+    if (receiveNotifications.value == false)
+    {
+        alert("Select one of the options!");
+        return false;
+    }
+    
+    return true;
+}
+
+function changeSecretQuestionValidation()
+{    
+    var currentSecretAnswer = document.forms["changeQuestionForm"]["currentSecretAnswer"];
+    var newSecretAnswer = document.forms["changeQuestionForm"]["newSecretAnswer"];
+    
+    if (currentSecretAnswer.value.length < 4 || currentSecretAnswer.value.length > 16)
+    {
+        alert("Invalid current secret answer!");
+        currentSecretAnswer.focus();
+        return false;
+    }
+    if (newSecretAnswer.value.length < 4 || newSecretAnswer.value.length > 16)
+    {
+        alert("New secret answer has to be 4 to 16 characters long!");
+        newSecretAnswer.focus();
+        return false;
+    }
+    
+    return true;
+}
+
+function validateEmail(email) 
+{
+    var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
 }
